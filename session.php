7d4f3c67d4640d3d81380a217a3cd1d0218fd0e0
madhavakannan <madhavakannan2010@gmail.com>
2019-04-11 11:42:51 +0530

    first commit by madhava

--- session.php
+++ session.php
@@ -6,6 +6,7 @@ session_start();
 <form action="text.php" method="POST">
 <input type="text" name="username"/><br>
 <input type="submit" name="submit" value="submit">
+<input type="submit" name="submit" value="ok">
 </form>
 </body>
 </html>
