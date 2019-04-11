44e97536ee5d6b69940a5d443cca7725af4924a1
madhavakannan <49508988+madhavakannan@users.noreply.github.com>
2019-04-11 11:23:06 +0530

    Add files via upload

---
+++ session.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+?>
+<html>
+<body>
+<form action="text.php" method="POST">
+<input type="text" name="username"/><br>
+<input type="submit" name="submit" value="submit">
+</form>
+</body>
+</html>
